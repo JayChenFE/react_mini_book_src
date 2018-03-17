1ddd22fa992e7f9f001ea95a50ef061e4869d11d
JayChenFE <chenjiehwy@163.com>
2018-03-17 15:57:42 +0800

    13

--- homework/13_黑色边框的容器组件.jsx
+++ homework/13_黑色边框的容器组件.jsx
@@ -2,3 +2,18 @@
 // .black - border{
 //     border: 1px solid #000000;
 // }
+
+class BlackBorderContainer extends Component {
+    /* TODO */
+    render() {
+        return (
+            <div>
+                {
+                    this.props.children.map(child => {
+                        return (<div className='black-border'>{child}</div>)
+                    }
+                    )}
+            </div>
+        )
+    }
+}
